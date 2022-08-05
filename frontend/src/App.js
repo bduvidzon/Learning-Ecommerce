75a6030208fd17ab6bd2ebb473bebf8321e895e0
Brian Duvidzon <bduvidzon@eagles.usi.edu>
2022-08-04 19:47:15 -0700

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href="/">Amazona</a>
+      </header>
+      <main>List of products</main>
+    </div>
+  );
+}
+
+export default App;
